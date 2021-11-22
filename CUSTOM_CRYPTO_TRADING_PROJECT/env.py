90d6ebbf1f680dd9291529b2521a6a5073353313
Roberto Lentini <rob.len001@gmail.com>
2021-11-22 11:32:41 -0500

    stable-baselines import issue

--- CUSTOM_CRYPTO_TRADING_PROJECT/env.py
+++ CUSTOM_CRYPTO_TRADING_PROJECT/env.py
@@ -11,10 +11,10 @@ import gym
 import random
 from gym import spaces
 import pandas as pd
-# import static
+import static
 
 
-class CryptoEnv(gym.env):
+class CryptoEnv(gym.Env):
     def __init__(self, data, title=None):
         '''Initializing the enviroment variables
 
@@ -87,11 +87,11 @@ class CryptoEnv(gym.env):
             '''
             # Get the data for the last 5 timestep
             frame = np.array([
-                self.df.loc[self.current_step - 4:self.current_step, 'Open'],
-                self.df.loc[self.current_step - 4:self.current_step, 'High'],
-                self.df.loc[self.current_step - 4:self.current_step, 'Low'],
-                self.df.loc[self.current_step - 4:self.current_step, 'Close'],
-                self.df.loc[self.current_step - 4:self.current_step, 'Volume'],
+                self.data.loc[self.current_step - 4:self.current_step, 'Open'],
+                self.data.loc[self.current_step - 4:self.current_step, 'High'],
+                self.data.loc[self.current_step - 4:self.current_step, 'Low'],
+                self.data.loc[self.current_step - 4:self.current_step, 'Close'],
+                self.data.loc[self.current_step - 4:self.current_step, 'Volume'],
             ])
             obs = frame
             return obs
@@ -106,8 +106,8 @@ class CryptoEnv(gym.env):
             # open and closed, the decision should be consitantly made
             # on close. This is riducolous to add randomness to this process
             current_price = random.uniform(
-                self.df.loc[self.current_step, 'Real open'],
-                self.df.loc[self.current_step, 'Real close'])
+                self.data.loc[self.current_step, 'Real open'],
+                self.data.loc[self.current_step, 'Real close'])
 
             # BUY
             if action[0] > 0:
@@ -135,3 +135,94 @@ class CryptoEnv(gym.env):
                 self.max_net_worth = self.net_worth
 
         def step(self, action, end=True):
+            '''Brings agent to a new env state
+
+            - action:  ???
+            - end: ???
+
+            Returns the new state
+            '''
+            self._take_action(action)
+            self.current_step += 1
+            # calculate the reward
+            # TODO: decide if you would like this reward function
+            # to be modified
+            profit = self.net_worth - (static.INITIAL_ACCOUNT_BALANCE +
+                static.BNBUSDTHELD)
+
+            profit_percent = profit / (static.INITIAL_ACCOUNT_BALANCE +
+                static.BNBUSDTHELD) * 100
+            # benchmark seems to be comparing to the price change of the crypto
+            # caculating how much more you would have made compared to just holding the currency
+            benchmark_profit = (self.data.loc[self.current_step, 'Real open']   / 
+            self.data.loc[self.start_step, 'Real close'] - 1) * 100
+            diff = profit_percent - benchmark_profit
+            # I do not love this reward function
+            reward = np.sign(diff) * (diff)**2
+            # setting a max amount of steps for an episode
+            if self.current_step >= static.MAX_STEPS + self.start_step:
+                end = True
+            else:
+                end = False
+            done = self.net_worth <= 0 or self.bnb_usdt_held <= 0 or end
+            if done and end:
+                self.episode_reward = reward
+                self._render_episode()
+                self.graph_profit.append(profit_percent)
+                self.graph_benchmark.append(benchmark_profit)
+                self.graph_reward.append(reward)
+                self.episode += 1
+            obs = self._next_observation()
+            # {} needed because gym wants 4 args
+            return obs, reward, done, {}
+
+        def render(self, print_step=False, graph=False, *args):
+            profit = self.net_worth - (static.INITIAL_ACCOUNT_BALANCE +
+                                    static.BNBUSDTHELD)
+
+            profit_percent = profit / (static.INITIAL_ACCOUNT_BALANCE +
+                                    static.BNBUSDTHELD) * 100
+
+            benchmark_profit = (self.df.loc[self.current_step, 'Real open'] /
+                                self.df.loc[self.start_step, 'Real open'] -
+                                1) * 100
+
+            if print_step:
+                print("----------------------------------------")
+                print(f'Step: {self.current_step}')
+                print(f'Balance: {round(self.balance, 2)}')
+                print(f'Crypto held: {round(self.crypto_held, 2)}')
+                print(f'Fees paid: {round(self.total_fees, 2)}')
+                print(f'Volume traded: {round(self.total_volume_traded, 2)}')
+                print(f'Net worth: {round(self.max_net_worth, 2)}')
+                print(f'Max net worth: {round(self.max_net_worth, 2)}')
+                print(f'Profit: {round(profit_percent, 2)}% ({round(profit, 2)})')
+                print(f'Benchmark profit: {round(benchmark_profit, 2)}')
+
+            # Plot the graph of the reward
+            if graph:
+                fig = plt.figure()
+                fig.suptitle('Training graph')
+
+                high = plt.subplot(2, 1, 1)
+                high.set(ylabel='Gain')
+                plt.plot(self.graph_profit, label='Bot profit')
+                plt.plot(self.graph_benchmark, label='Benchmark profit')
+                high.legend(loc='upper left')
+
+                low = plt.subplot(2, 1, 2)
+                low.set(xlabel='Episode', ylabel='Reward')
+                plt.plot(self.graph_reward, label='reward')
+
+                plt.show()
+
+            return profit_percent, benchmark_profit
+
+        def _render_episode(self, filename='render/render.txt'):
+            file = open(filename, 'a')
+            file.write('-----------------------\n')
+            file.write(f'Episode numero: {self.episode}\n')
+            file.write(f'Profit: {round(self.render()[0], 2)}%\n')
+            file.write(f'Benchmark profit: {round(self.render()[1], 2)}%\n')
+            file.write(f'Reward: {round(self.episode_reward, 2)}\n')
+            file.close()
---
+++ CUSTOM_CRYPTO_TRADING_PROJECT/static.py
@@ -0,0 +1,11 @@
+'''Constants for the env
+
+    author: Roberto Lentini
+    email: roberto.lentini@mail.utoronto.ca
+    date: November 22nd 2021
+'''
+BNBUSDTHELD = 0
+INITIAL_ACCOUNT_BALANCE = 100
+MAX_STEPS = 100
+MAKER_FEE = 0.1
+TAKER_FEE = 0.1
